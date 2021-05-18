dfff151484366de9102339d6abdd23f00adaa172
EveraldoTorres1984 <torre@192.168.15.8>
2021-05-18 18:32:40 -0300

    Atividade aula 07 vetores

---
+++ src/main/java/com/mycompany/ativ_aula07/Principal.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.ativ_aula07;
+
+/**
+ *
+ * @author torre
+ */
+public class Principal {
+    public static void main(String[] args) {
+        Disciplinas disc = new Disciplinas();
+        disc.atividade01();
+    }
+    
+}
